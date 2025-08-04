ef159d60359423008153838661327d525cd7212b
hauntedVCR <142163550+hauntedVCR@users.noreply.github.com>
2025-08-04 00:13:11 -0400

    Update MasterList.js
    Added new potential slot items

--- Scripts/MasterList.js
+++ Scripts/MasterList.js
@@ -51,5 +51,10 @@ unmodList=[
 {slot:"Paw item", found: false},
 {slot:"Kin is a recolor", found: false},
 {slot:"Rectangle silhouette", found: false},
-{slot:"Angels/Demons theme", found: false}
+{slot:"Angels/Demons theme", found: false},
+{slot:"New item is based on popular new media", found: false},
+{slot:"New item is based on folklore", found: false},
+{slot:"Umami Drop style release", found: false},
+{slot:"New Prisma item", found: false}
 ];
+
